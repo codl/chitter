608a21e906b0800195cd50b36fd89dc816443561
codl <codl@codl.fr>
2017-04-20 07:28:09 +0200

    whoops forgot to remove a superfluous amiga test

--- app/assets/javascripts/components/reducers/timelines.jsx
+++ app/assets/javascripts/components/reducers/timelines.jsx
@@ -86,7 +86,6 @@ const initialState = Immutable.Map({
 const normalizeStatus = (state, status) => {
   const replyToId = status.get('in_reply_to_id');
   const id        = status.get('id');
-  const amiga = /\ba+mi+gaa+\b/ig.test(status.get('content'));
 
   if (replyToId) {
     if (!state.getIn(['descendants', replyToId], Immutable.List()).includes(id)) {
