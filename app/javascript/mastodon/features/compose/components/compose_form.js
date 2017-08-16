2ab0ad45fce91bab86c104db7d8b076ca2b4ae65
codl <codl@codl.fr>
2017-08-16 17:35:41 +0200

    change toot button to boop

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -23,8 +23,8 @@ import { countableText } from '../util/counter';
 const messages = defineMessages({
   placeholder: { id: 'compose_form.placeholder', defaultMessage: 'What is on your mind?' },
   spoiler_placeholder: { id: 'compose_form.spoiler_placeholder', defaultMessage: 'Write your warning here' },
-  publish: { id: 'compose_form.publish', defaultMessage: 'Toot' },
-  publishLoud: { id: 'compose_form.publish_loud', defaultMessage: '{publish}!' },
+  publish: { id: 'chitter.compose_form.publish', defaultMessage: 'Boop' },
+  publishLoud: { id: 'chitter.compose_form.publish_loud', defaultMessage: '{publish}!' },
 });
 
 @injectIntl
