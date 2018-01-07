25b8c2d75b29e77c24fe3d8652900b102841c068
codl <codl@codl.fr>
2018-01-07 22:54:25 +0100

    improve mascot crediting, now has a link and can go to an account

--- app/javascript/mastodon/features/compose/components/mascot.js
+++ app/javascript/mastodon/features/compose/components/mascot.js
@@ -1,32 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Mascot extends React.Component {
+
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+
   constructor () {
     super();
-    this.state = {};
+    this.state = {mascot: null};
+    this.go_to_credit = this.go_to_credit.bind(this);
   }
 
   componentDidMount () {
-    fetch('/mascots.json').then(response => response.json())
+    fetch('https://media.chitter.xyz/mascots/mascots.json?2').then(response => response.json())
       .then(mascots=>{
         this.setState({mascot: mascots[Math.floor(Math.random()*mascots.length)]});
       });
   }
 
-  render () {
-    let mascot_style = {};
-    if (this.state.mascot) {
-      mascot_style = {
-        backgroundImage: 'url('+this.state.mascot.url+')'
-      };
+  go_to_credit (e) {
+    if (!this.state.mascot || !this.state.mascot.credit || !this.state.mascot.credit.account_id ||
+        e.button !== 0){
+      return
+    }
+    e.preventDefault();
+    this.context.router.history.push(`/accounts/${this.state.mascot.credit.account_id}`);
+  }
 
+  render () {
+    if (!this.state.mascot) {
+      return false
+    }
+    let mascot_style = {
+      backgroundImage: 'url('+this.state.mascot.url+')'
+    };
+
+    let credit = this.state.mascot.credit.name;
+    if (this.state.mascot.credit.url){
+      credit = <a href={this.state.mascot.credit.url} onClick={this.go_to_credit}>
+        {this.state.mascot.credit.name}
+      </a>
     }
 
     return (
       <div className='mascot' style={mascot_style}>
-        { this.state.mascot &&
-          <p className='mascot-credit'>Mascot drawn by {this.state.mascot.credit}</p>
-        }
+        <p className='mascot-credit'>
+          Mascot drawn by {credit}
+        </p>
       </div>
     )
   }
