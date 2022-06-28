6bac13113158f410b18ed161fd014339aa4a8121
Claire <claire.github-309c@sitedethib.com>
2022-06-28 09:42:13 +0200

    Revamp post filtering system (#18058)
    * Add model for custom filter keywords
    
    * Use CustomFilterKeyword internally
    
    Does not change the API
    
    * Fix /filters/edit and /filters/new
    
    * Add migration tests
    
    * Remove whole_word column from custom_filters (covered by custom_filter_keywords)
    
    * Redesign /filters
    
    Instead of a list, present a card that displays more information and handles
    multiple keywords per filter.
    
    * Redesign /filters/new and /filters/edit to add and remove keywords
    
    This adds a new gem dependency: cocoon, as well as a npm dependency:
    cocoon-js-vanilla. Those are used to easily populate and remove form fields
    from the user interface when manipulating multiple keyword filters at once.
    
    * Add /api/v2/filters to edit filter with multiple keywords
    
    Entities:
    - `Filter`: `id`, `title`, `filter_action` (either `hide` or `warn`), `context`
      `keywords`
    - `FilterKeyword`: `id`, `keyword`, `whole_word`
    
    API endpoits:
    - `GET /api/v2/filters` to list filters (including keywords)
    - `POST /api/v2/filters` to create a new filter
      `keywords_attributes` can also be passed to create keywords in one request
    - `GET /api/v2/filters/:id` to read a particular filter
    - `PUT /api/v2/filters/:id` to update a new filter
      `keywords_attributes` can also be passed to edit, delete or add keywords in
       one request
    - `DELETE /api/v2/filters/:id` to delete a particular filter
    - `GET /api/v2/filters/:id/keywords` to list keywords for a filter
    - `POST /api/v2/filters/:filter_id/keywords/:id` to add a new keyword to a
       filter
    - `GET /api/v2/filter_keywords/:id` to read a particular keyword
    - `PUT /api/v2/filter_keywords/:id` to edit a particular keyword
    - `DELETE /api/v2/filter_keywords/:id` to delete a particular keyword
    
    * Change from `irreversible` boolean to `action` enum
    
    * Remove irrelevent `irreversible_must_be_within_context` check
    
    * Fix /filters/new and /filters/edit with update for filter_action
    
    * Fix Rubocop/Codeclimate complaining about task names
    
    * Refactor FeedManager#phrase_filtered?
    
    This moves regexp building and filter caching to the `CustomFilter` class.
    
    This does not change the functional behavior yet, but this changes how the
    cache is built, doing per-custom_filter regexps so that filters can be matched
    independently, while still offering caching.
    
    * Perform server-side filtering and output result in REST API
    
    * Fix numerous filters_changed events being sent when editing multiple keywords at once
    
    * Add some tests
    
    * Use the new API in the WebUI
    
    - use client-side logic for filters we have fetched rules for.
      This is so that filter changes can be retroactively applied without
      reloading the UI.
    - use server-side logic for filters we haven't fetched rules for yet
      (e.g. network error, or initial timeline loading)
    
    * Minor optimizations and refactoring
    
    * Perform server-side filtering on the streaming server
    
    * Change the wording of filter action labels
    
    * Fix issues pointed out by linter
    
    * Change design of “Show anyway” link in accordence to review comments
    
    * Drop “irreversible” filtering behavior
    
    * Move /api/v2/filter_keywords to /api/v1/filters/keywords
    
    * Rename `filter_results` attribute to `filtered`
    
    * Rename REST::LegacyFilterSerializer to REST::V1::FilterSerializer
    
    * Fix systemChannelId value in streaming server
    
    * Simplify code by removing client-side filtering code
    
    The simplifcation comes at a cost though: filters aren't retroactively
    applied anymore.

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -118,6 +118,7 @@ class Status extends ImmutablePureComponent {
   state = {
     showMedia: defaultMediaVisibility(this.props.status),
     statusId: undefined,
+    forceFilter: undefined,
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -279,6 +280,15 @@ class Status extends ImmutablePureComponent {
     this.handleToggleMediaVisibility();
   }
 
+  handleUnfilterClick = e => {
+    this.setState({ forceFilter: false });
+    e.preventDefault();
+  }
+
+  handleFilterClick = () => {
+    this.setState({ forceFilter: true });
+  }
+
   _properStatus () {
     const { status } = this.props;
 
@@ -330,7 +340,8 @@ class Status extends ImmutablePureComponent {
       );
     }
 
-    if (status.get('filtered') || status.getIn(['reblog', 'filtered'])) {
+    const matchedFilters = status.get('filtered') || status.getIn(['reblog', 'filtered']);
+    if (this.state.forceFilter === undefined ? matchedFilters : this.state.forceFilter) {
       const minHandlers = this.props.muted ? {} : {
         moveUp: this.handleHotkeyMoveUp,
         moveDown: this.handleHotkeyMoveDown,
@@ -339,7 +350,11 @@ class Status extends ImmutablePureComponent {
       return (
         <HotKeys handlers={minHandlers}>
           <div className='status__wrapper status__wrapper--filtered focusable' tabIndex='0' ref={this.handleRef}>
-            <FormattedMessage id='status.filtered' defaultMessage='Filtered' />
+            <FormattedMessage id='status.filtered' defaultMessage='Filtered' />: {matchedFilters.join(', ')}.
+            {' '}
+            <button className='status__wrapper--filtered__button' onClick={this.handleUnfilterClick}>
+              <FormattedMessage id='status.show_filter_reason' defaultMessage='Show anyway' />
+            </button>
           </div>
         </HotKeys>
       );
@@ -498,7 +513,7 @@ class Status extends ImmutablePureComponent {
 
             {media}
 
-            <StatusActionBar scrollKey={scrollKey} status={status} account={account} {...other} />
+            <StatusActionBar scrollKey={scrollKey} status={status} account={account} onFilter={matchedFilters && this.handleFilterClick} {...other} />
           </div>
           <ConfettiOverlay status={status} />
         </div>
